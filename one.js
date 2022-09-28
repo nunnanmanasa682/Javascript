9e99a29728aec0938557fa43912a25db54f1bda4
nunnanmanasa682 <nunnanmanasa682@gmail.com>
2022-09-28 12:07:28 +0530

    okey

---
+++ one.js
@@ -0,0 +1,29 @@
+var a=10;
+var b=20;
+var c="thirty";
+var d;
+var e=40.5;
+var f=true;
+var g="rahul";
+var h="gandhi"
+var i="false";
+var j="30";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a+c);
+console.log(a*c);
+console.log(a+d);
+console.log(a*d);
+console.log(a+e);
+console.log(a*e);
+console.log(a+f);
+console.log(a*f);
+console.log(a+g);
+console.log(a*g);
+console.log(a+h);
+console.log(a*h);
+console.log(a+i);
+console.log(a*i);
+console.log(a+j);
+console.log(a*j);
