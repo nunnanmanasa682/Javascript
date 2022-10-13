e4de9296183cfb76e1b67732f01626727e5f480f
nunnanmanasa682 <nunnanmanasa682@gmail.com>
2022-10-13 17:22:54 +0530

    okey

--- Dom/data.js
+++ Dom/data.js
@@ -1,3 +1,18 @@
 let employees=[
-    {id:101,name:"manasa",email: mani@.c,salary: 45000},
+    {id:101,name:"manasa",salary: 45000},
+    {id:102,name:"manas",salary: 34000},
+    {id:103,name:"manu",salary: 65000},
+    {id:104,name:"manisha",salary: 30000},
+    {id:1015,name:"mani",salary: 35000}
 ]
+function display_Data(){
+    let rows=""
+    for(employee of empoyees){
+    rows=rows +  `<tr>
+                  <td>${employees.id}</td>
+                  <td>${employees.name}</td>
+                  <td>${employees.salary}</td>
+                  <tr>`   
+    }
+    document.getElementById('abc').innerHTML=rows
+}
