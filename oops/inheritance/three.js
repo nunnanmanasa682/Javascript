a7efc6f391728a83f25f54c4681040c96da15810
nunnanmanasa682 <nunnanmanasa682@gmail.com>
2022-10-19 16:35:04 +0530

    okey

--- oops/inheritance/three.js
+++ oops/inheritance/three.js
@@ -1,5 +1,15 @@
-<HTML>
-    <head>
-        <title></title>
-    </head>
-</HTML>
+class Account{
+    constructor(name,email){
+    this.acc_name=name;
+    this.acc_email=email;
+}
+}
+class saving_Account extends Account{
+    constructor(id,name,email,amount){
+        super(name,email);
+        this.acc_id=id;
+        this.acc_amount=amount
+    }
+}
+let a1=new saving_Account(101,"manasa","nunn@gamil.com",20000)
+console.log(a1)
--- oops/inheritance/two.js
+++ oops/inheritance/two.js
@@ -1,21 +1,24 @@
-class Account{
+class Account
+{
     min_Bal=500;
     get_Account_details(){
         console.log("diplaying account");
     }
 }
 class Saving_Account extends Account{
-    deposit_amount(amount){
+    deposite_Amount(amount){
         console.log(this.min_Bal+amount);
     }
 }
  let a1= new Saving_Account()
 a1.get_Account_details() 
-console.log(a1.min_bal);
-a1.deposit_amount(500)
+console.log(a1.min_Bal);
+a1.deposite_Amount(1500)
+
 class current_Account extends Account{
     min_Bal=10000;
-    deposit_amount(amount){
+    deposite_amount(amount){
         console.log(this.min_Bal+amount);
     }
+
 }
